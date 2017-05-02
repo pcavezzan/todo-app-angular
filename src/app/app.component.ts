c76f18a55e1f047545d08bd5cbd2fd2fe32f3306
pcavezzan <pcavezzan@gmail.com>
2017-05-02 09:06:00 +0200

    Step1: Getting 1st version TodoApp up and running
    Adding user interaction and test inside the component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TodoDataService } from './todo-data.service';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,25 @@ import { TodoDataService } from './todo-data.service';
 })
 export class AppComponent {
   title = 'app works!';
+  newTodo :Todo = new Todo({});
 
   constructor(private todoDataService: TodoDataService) {
   }
 
+  public toggleTodoComplete(todo :Todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  addTodo() {
+    this.todoDataService.add(this.newTodo);
+    this.newTodo = new Todo({});
+  }
+
+  removeTodo(todo :Todo) {
+    this.todoDataService.delete(todo.id);
+  }
+
+  get todos() :Todo[] {
+    return this.todoDataService.findAll();
+  }
 }
