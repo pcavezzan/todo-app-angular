76929366cb47572102734cb4c1f2e62eb0ac1193
pcavezzan <pcavezzan@gmail.com>
2017-05-03 00:56:51 +0200

    Added style (same from tutorial)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from '@angular/material';
+import 'hammerjs';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,6 +14,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    MaterialModule.forRoot(),
+    BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
     HttpModule
